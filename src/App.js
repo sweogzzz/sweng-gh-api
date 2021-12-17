47d2f62625eb91493ead50460e0ff3a9c85fa5c5
sweogzzz <sweogzzz>
2021-12-17 10:56:14 +0000

    github api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  submitHandle(e) {
+    e.preventDefault();
+
+    // github personal token
+    const token = '';
+
+    this.setState({
+      repo:'',
+      lans:'',
+      cons:'',
+      coms:''
+    })
+
+    var q = e.target.query.value;
+    if(!(/[a-zA-Z0-9]*\/[a-zA-Z0-9]*/.test(q))) return;
+    
+    axios.get('https://api.github.com/repos/'+q,
+    {headers:{'Authorization':`token ${token}`}})
+    .then(response => this.setState({repo:response.data}))
+    .catch((err) => {console.log(err)});
+  }
+  componentDidMount() {
+    document.title = "Metrics"
+  }
+  constructor(args) {
+    super(args);
+    this.state = {
+      repo:'',
+      lans:'',
+      cons:'',
+      coms:''
+    }
+    this.submitHandle = this.submitHandle.bind(this);
+  }
+  render() {
+    return (
+      <div>
+        <h1>GitHub Metrics</h1>
+        <form onSubmit={this.submitHandle}>
+          <label>
+            <input
+              name="query"
+              type="text"
+              placeholder="user/repo"
+              required
+            />
+          </label>
+          <br></br>
+          <input
+            type="submit"
+            value="Fetch"
+          />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
