0b9ca34933040f0bd5238d6411650283d7a6f52f
Owen Gallagher <sweogzzz@gmail.com>
2021-12-17 11:55:00 +0000

    line graph operations

--- src/App.js
+++ src/App.js
@@ -52,6 +52,32 @@ class App extends React.Component {
     this.submitHandle = this.submitHandle.bind(this);
   }
   render() {
+    var coms1 = [];
+    var coms2 = [];
+    for (i in this.state.coms) {
+      a = this.state.coms[i].commit.author.date;
+      b = new Date(a.substring(0,10));
+      coms1.push(b.getTime());
+    }
+    for (i in coms1) {
+      if (coms2[i]) coms2[i] += 1;
+      else coms2[i] = 1;
+    }
+    coms1 = [...new Set(coms1)].sort()
+    var coms3 = coms1.map(a => coms2[a]);
+    var coms4 = coms3.map((a => b => a+=b)(0));
+    var comsf = {
+      labels: coms1,
+      datasets: [{
+        label: 'total commits',
+        data: coms3,
+        borderWidth: 1
+      },{
+        label: 'new commits',
+        data: coms4,
+        borderWidth: 1
+      }]
+    }
     return (
       <div>
         <h1>GitHub Metrics</h1>
