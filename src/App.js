e1b89c868d070ed665f0a839fe9e3f47d08d0445
sweogzzz <sweogzzz>
2021-12-17 18:13:28 +0000

    polish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import 'date-fns';
 import 'chartjs-adapter-date-fns';
@@ -6,17 +6,17 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
+  BarElement,
   Title,
   Tooltip,
   Legend,
-  BarElement,
   ArcElement,
   LineElement,
   PointElement,
   RadialLinearScale,
   TimeScale
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { Doughnut, Line, Radar } from 'react-chartjs-2';
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -29,24 +29,25 @@ ChartJS.register(
   Legend,
   RadialLinearScale,
   TimeScale
-)
+);
 
-class App extends React.Component {
-  submitHandle(e) {
+class App extends Component {
+  submitHandler(e) {
     e.preventDefault();
-
-    // github personal token
     const token = process.env.REACT_APP_SECRET;
 
     this.setState({
       repo: '',
       lans: '',
       cons: '',
-      coms: ''
-    })
+      coms: '',
+      name: ''
+    });
 
     var q = e.target.query.value;
-    if (!(/[a-zA-Z0-9]*\/[a-zA-Z0-9]*/.test(q))) return;
+    if (!(/[a-zA-Z0-9]+\/[a-zA-Z0-9]+/.test(q))) return;
+
+    this.setState({name:q.split('/')[0]})
 
     axios.get('https://api.github.com/repos/' + q,
       { headers: { 'Authorization': `token ${token}` } })
@@ -71,87 +72,116 @@ class App extends React.Component {
   componentDidMount() {
     document.title = "Metrics"
   }
-  constructor(args) {
-    super(args);
+  constructor(props) {
+    super(props);
     this.state = {
       repo: '',
       lans: '',
       cons: '',
-      coms: ''
+      coms: '',
+      name: ''
     }
-    this.submitHandle = this.submitHandle.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
   }
   render() {
-    var i;
-    var a;
-    var b;
-    var coms1 = [];
-    var coms2 = [];
-    for (i in this.state.coms) {
-      a = this.state.coms[i].commit.author.date;
-      b = new Date(a);
-      coms1.push(b.getTime());
+
+    var forks = parseInt(this.state.repo.forks);
+    forks = forks > 100 ? 5 : forks > 50 ? 4 : forks > 25 ? 3 : forks > 10 ? 2 : forks > 5 ? 1 : 1;
+    var issues = parseInt(this.state.repo.open_issues);
+    issues = issues > 100 ? 5 : issues > 10 ? 4 : issues > 5 ? 3 : issues > 2 ? 2 : issues > 1 ? 1 : 1;
+    var watching = parseInt(this.state.repo.watchers);
+    watching = watching > 10 ? 5 : watching > 5 ? 4 : watching > 3 ? 2 : watching > 1 ? 1 : 1
+    var size = parseInt(this.state.repo.size);
+    size = size > 100000 ? 5 : size > 40000 ? 4 : size > 4000 ? 3 : size > 300 ? 2 : size > 100 ? 1 : 1;
+
+    var comsd = [];
+    var comsn = {};
+    for (var i in this.state.coms) {
+      var dtxt = this.state.coms[i].commit.author.date;
+      comsd.push((new Date(dtxt)).getTime());
     }
-    for (i of coms1) {
-      if (coms2[i]) coms2[i] += 1;
-      else coms2[i] = 1;
+    comsd.sort();
+    for (let e of comsd) {
+      if (comsn[e]) comsn[e] += 1
+      else comsn[e] = 1;
     }
-    coms1 = coms1.sort();
-    coms1 = [...new Set(coms1)];
-    var coms3 = coms1.map(a => coms2[a]);
-    var coms4 = coms3.map((a => b => a += b)(0));
-    var comsf = {
-      labels: coms1,
+    comsd = [...new Set(comsd)];
+    var comsv = comsd.map(a => comsn[a]);
+    var comsv2 = comsv.map((a => b => a += b)(0));
+    var comsdata = {
+      labels: comsd,
       datasets: [{
-        label: 'new commits',
-        data: coms3,
-        borderWidth: 1,
-        borderColor: 'blue',
-        cubicInterpolationMode: 'monotone',
-        tension: 0.4
-      }, {
-        label: 'total commits',
-        data: coms4,
-        borderWidth: 1,
+        label: 'overall',
+        data: comsv2,
+        fill: true,
+        backgroundColor: 'red',
         borderColor: 'red',
+        borderWidth: 1,
         cubicInterpolationMode: 'monotone',
-        tension: 0.4
+        tension:0.6
       }]
     }
-    var lans1 = Object.keys(this.state.lans);
-    var lans2 = Object.values(this.state.lans);
-    lans1.length = lans1.length > 10 ? 10 : lans1.length;
-    lans2.length = lans2.length > 10 ? 10 : lans2.length;
-    lans2 = lans2.map(a => parseInt(a));
-    var lansf = {
-      labels: lans1,
+
+    var langsnm = Object.keys(this.state.lans);
+    var langspop = Object.values(this.state.lans);
+    langspop = langspop.map(a => parseInt(a));
+    langsnm.length = langsnm.length > 10 ? 10 : langsnm.length;
+    langspop.length = langspop.length > 10 ? 10 : langspop.length;
+    if (langsnm.length === 0) {
+      langsnm = ['unidentified']
+      langspop = [100]
+    }
+    var langsdata = {
+      labels: langsnm,
       datasets: [{
         label: '% of code',
-        data: lans2.map(a => a * 100 / (lans2.reduce((a, b) => a + b, 0))),
-        backgroundColor: ['#f72585', '#b5179e', '#7209b7', '#560bad',
-          '#480ca8', '3a0ca3', '#3f37c9', '#3f37c9', '#4361ee', '#4cc9f0']
+        data: langspop.map(a => a * 100 / (langspop.reduce((a, b) => a + b, 0))),
+        backgroundColor: ['#f72585', '#b5179e', '#7209b7', '#560bad', '#480ca8', '3a0ca3', '#3f37c9', '#3f37c9', '#4361ee', '#4cc9f0']
       }]
     }
-    var cons1 = [];
-    var cons2 = [];
-    var cons3 = 0;
+
+    var teammems = [];
+    var teamctrb = [];
+    var teamcnt = 0;
     for (i in this.state.cons) {
-      cons1.push(this.state.cons[i].login);
-      cons2.push(this.state.cons[i].contributions);
-      if (cons3++ >= 10) break;
+      teammems.push(this.state.cons[i].login);
+      teamctrb.push(this.state.cons[i].contributions);
+      teamcnt++;
+      if (teamcnt >= 10) break;
     }
-    cons2 = cons2.map(a => parseInt(a));
-    var consf = {
-      labels: cons1,
-      data: cons2.map(a => a * 100 / (cons2.reduce((a, b) => a + b, 0))),
-      borderWidth: 1,
-      backgroundColor: ['#001219', '#005f73', '#0a9396', '#94d2bd',
-        '#e9d8a6', 'ee9b00', '#ca6702', '#bb3e03', '#ae2012', '#9b2226']
+    teamctrb = teamctrb.map(a => parseInt(a));
+    if (teamcnt === 0) {
+      teamctrb = [100]
+      teammems = [this.state.name]
     }
+    var teamdata = {
+      labels: teammems,
+      datasets: [{
+        label: '% contrib',
+        data: teamctrb.map(a => a * 100 / (teamctrb.reduce((a, b) => a + b, 0))),
+        borderWidth: 1,
+        backgroundColor: ['#001219', '#005f73', '#0a9396', '#94d2bd', '#e9d8a6', 'ee9b00', '#ca6702', '#bb3e03', '#ae2012', '#9b2226']
+      }]
+    }
+
+    var teamctrb2 = teamctrb.reduce((a, b) => a + b, 0);
+    teamctrb2 = teamctrb2 > 200 ? 5 : teamctrb2 > 150 ? 4 : teamctrb2 > 100 ? 3 : teamctrb2 > 10 ? 2 : teamctrb2 > 0 ? 1 : 1
+
+    var rdata = {
+      labels: ['forks', 'issues', 'watching', 'size', 'commits'],
+      datasets: [{
+        label: 'this repo',
+        data: [forks, issues, watching, size, teamctrb2],
+        backgroundColor: 'red',
+        borderColor: 'red'
+      }]
+    }
+
     return (
       <div>
-        <h1>GitHub Metrics</h1>
-        <form onSubmit={this.submitHandle}>
+        <h1>GitHub API</h1>
+        <br></br>
+        <form onSubmit={this.submitHandler}>
           <label>
             <input
               name="query"
@@ -160,24 +190,57 @@ class App extends React.Component {
               required
             />
           </label>
-          <br></br>
-          <input
-            type="submit"
-            value="Fetch"
-          />
+          <div>
+            <input
+              type="submit"
+              value="Fetch"
+            />
+          </div>
         </form>
-        <h2>{
-          this.state.repo ?
-            this.state.repo.name : ''
-        }</h2>
-        <h3>{
-          this.state.repo ?
-            this.state.repo.owner.login : ''
-        }</h3>
+        <br></br>
+        <h4>Owner: {this.state.name}</h4>
+        <img width="100" src={this.state.repo ? this.state.repo.owner.avatar_url : null} alt="avatar.png" />
         <div>
+          <h5>
+            Repository stats
+          </h5>
+          <p>
+            1-5 scale based on min/max figures
+          </p>
           <div id="frame">
-            {this.state.coms?<Line
-              data={comsf}
+            {this.state.repo? <Radar
+              data={rdata}
+              options={{
+                maintainAspectRatio: false,
+                reponsive: false,
+                plugins: {
+                  title: {
+                    display: false
+                  },
+                  legend: {
+                    display: false
+                  }
+                },
+                scale: {
+                  ticks: {
+                    stepSize: 1
+                  },
+                  min:0,
+                  max:5
+                }
+              }}
+              height={200}
+            /> : 'Stats not found' }
+          </div>
+          <h5>
+            Recent commits to repo
+          </h5>
+          <p>
+            Recent timeline of commits
+          </p>
+          <div id="frame">
+            {this.state.coms? <Line
+              data={comsdata}
               options={{
                 elements: {
                   point: {
@@ -187,29 +250,69 @@ class App extends React.Component {
                 plugins: {
                   title: {
                     display: false
+                  },
+                  legend: {
+                    display: false
                   }
+                },
+                interaction: {
+                  intersect: false,
                 },
                 scales: {
                   x: {
                     type: 'time',
                     time: {
-                      unit: 'hour'
+                      unit: ''
                     }
                   }
-                },
-                interaction: {
-                  intersect: false
                 }
-              }
-            }
-            height={200}
-            /> : 'No data'}
+              }}
+            /> : 'No commits found'}
           </div>
+          <h5>
+            Contributors to repo
+          </h5>
+          <p>
+            Distribution of commits by user
+          </p>
           <div id="frame">
-            { }
+            {this.state.cons? <Doughnut
+              data={teamdata}
+              options={{
+                maintainAspectRatio: false,
+                plugins: {
+                  title: { display: false },
+                  legend: { display: true, position: 'bottom' }
+                },
+                elements: {
+                  arc: {
+                    borderWidth: 0
+                  }
+                }
+              }}
+              height={240}
+            /> : 'No contributions found'}
           </div>
+          <h5>
+            Languages used in repo
+          </h5>
           <div id="frame">
-            { }
+            {this.state.lans? <Doughnut
+              data={langsdata}
+              options={{
+                maintainAspectRatio: false,
+                plugins: {
+                  title: { display: false },
+                  legend: { display: true, position: 'bottom' }
+                },
+                elements: {
+                  arc: {
+                    borderWidth: 0
+                  }
+                }
+              }}
+              height={240}
+            /> : 'No languages identified'}
           </div>
         </div>
       </div>
